9291e45d371daf7dcd95fd4bf62e2e82a7bdbb30
root <root@hadoop-btdn9001.i1fjeftrltcetglym4fv3msarh.cx.internal.chinacloudapp.cn>
2021-05-17 10:21:02 +0000

    modify restart.sh

--- bin/restart.sh
+++ bin/restart.sh
@@ -3,11 +3,10 @@
 cd ../target/
 cp spark-ranger-1.0-SNAPSHOT.jar /usr/local/hadoop/spark-release/jars/
 
-cd ../sbin/
-./stop-thriftserver.sh
+/usr/local/hadoop/spark-release/sbin/stop-thriftserver.sh
 sleep 5
-./start-thriftserver.sh --deploy-mode client --hiveconf hive.server2.thrift.port=14000 --hiveconf hive.server2.authentication.kerberos.keytab=/etc/security/keytab/eos.keytab --hiveconf hive.server2.authentication.kerberos.principal=eos@ENIOT.IO
-sleep 8
+/usr/local/hadoop/spark-release/sbin/start-thriftserver.sh --deploy-mode client --hiveconf hive.server2.thrift.port=14000 --hiveconf hive.server2.authentication.kerberos.keytab=/etc/security/keytab/eos.keytab --hiveconf hive.server2.authentication.kerberos.principal=eos@ENIOT.IO
+sleep 60
 
-cd ../bin/
+cd /usr/local/hadoop/spark-release/bin
 beeline -u "jdbc:hive2://localhost:14000/wall"

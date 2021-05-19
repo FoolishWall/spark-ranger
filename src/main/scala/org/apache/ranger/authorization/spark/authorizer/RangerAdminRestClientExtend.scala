a421070424627fe5d9d355d36eff44cff1857675
kobe0024 <798657709@qq.com>
2021-05-19 15:15:29 +0800

    optimize Authorization

--- src/main/scala/org/apache/ranger/authorization/spark/authorizer/RangerAdminRestClientExtend.scala
+++ src/main/scala/org/apache/ranger/authorization/spark/authorizer/RangerAdminRestClientExtend.scala
@@ -15,33 +15,38 @@ import sun.misc.BASE64Encoder
  * @description:
  * */
 class RangerAdminRestClientExtend extends RangerAdminRESTClient {
+
+  import RangerAdminRestClientExtend._
+
   private val LOG: Log = LogFactory.getLog(classOf[RangerAdminRestClientExtend])
 
   private var serviceName = ""
   private var appId = ""
   private var pluginId = ""
   private var clusterName = ""
+  private var username = ""
+  private var password = ""
   private var restClient = new RangerRESTClient
   private val restUtils = new RangerRESTUtils
 
   override def init(serviceName: String, appId: String, propertyPrefix: String): Unit = {
-    super.init(serviceName, appId, propertyPrefix)
     //get parameter
+    super.init(serviceName, appId, propertyPrefix)
+
+    //set parameter
     this.serviceName = serviceName
     this.appId = appId
     this.pluginId = this.restUtils.getPluginId(serviceName, appId);
     this.clusterName = RangerConfiguration.getInstance.get(propertyPrefix + ".ambari.cluster.name", "")
+    this.username = RangerConfiguration.getInstance.get(propertyPrefix + ".username")
+    this.password = RangerConfiguration.getInstance.get(propertyPrefix + ".password")
 
     //init RangerRESTClient
     var url = ""
     val tmpUrl = RangerConfiguration.getInstance.get(propertyPrefix + ".policy.rest.url")
-    LOG.info("*** tmpUrl ***" + tmpUrl)
     val sslConfigFileName = RangerConfiguration.getInstance.get(propertyPrefix + ".policy.rest.ssl.config.file")
-    LOG.info("*** sslConfigFileName ***" + sslConfigFileName)
     val restClientConnTimeOutMs = RangerConfiguration.getInstance.getInt(propertyPrefix + ".policy.rest.client.connection.timeoutMs", 120000)
-    LOG.info("*** restClientConnTimeOutMs ***" + restClientConnTimeOutMs)
     val restClientReadTimeOutMs = RangerConfiguration.getInstance.getInt(propertyPrefix + ".policy.rest.client.read.timeoutMs", 30000)
-    LOG.info("*** restClientReadTimeOutMs ***" + restClientReadTimeOutMs)
     if (!StringUtil.isEmpty(tmpUrl)) url = tmpUrl.trim
     if (url.endsWith("/")) url = url.substring(0, url.length - 1)
     this.restClient = new RangerRESTClient(url, sslConfigFileName)
@@ -50,26 +55,23 @@ class RangerAdminRestClientExtend extends RangerAdminRESTClient {
   }
 
   override def getServicePoliciesIfUpdated(lastKnownVersion: Long, lastActivationTimeInMillis: Long): ServicePolicies = {
-    LOG.info("=========start get policy=========")
-    LOG.info("***lastKnownVersion***" + lastKnownVersion)
-    LOG.info("***lastActivationTimeInMillis***" + lastActivationTimeInMillis)
-    LOG.info("***serviceName***" + this.serviceName)
-    LOG.info("***PluginId***" + this.pluginId)
-    LOG.info("***ClusterName***" + this.clusterName)
-
-    val name = "admin"
-    val password = "8JJFgzTV"
-    val authString = name + ":" + password
+    LOG.info("===============start get policy===============")
+    //authorisationToken
+    val authString = this.username + ":" + this.password
     val authStringEnc = new BASE64Encoder().encode(authString.getBytes)
 
-    val webResource = this.restClient.getResource("/service/plugins/secure/policies/download/" + this.serviceName)
-        .queryParam("lastKnownVersion", lastKnownVersion.toString)
-        .queryParam("lastActivationTime", lastActivationTimeInMillis.toString)
-        .queryParam("pluginId", this.pluginId).queryParam("clusterName", this.clusterName)
-    val response = webResource.accept("application/json").header("Authorization", "Basic " + authStringEnc).get(classOf[ClientResponse])
+    val webResource = this.restClient.getResource(REST_URL_POLICY_GET_FOR_SECURE_SERVICE_IF_UPDATED + this.serviceName)
+        .queryParam(REST_PARAM_LAST_KNOWN_POLICY_VERSION, lastKnownVersion.toString)
+        .queryParam(REST_PARAM_LAST_ACTIVATION_TIME, lastActivationTimeInMillis.toString)
+        .queryParam(REST_PARAM_PLUGIN_ID, this.pluginId)
+        .queryParam(REST_PARAM_CLUSTER_NAME, this.clusterName)
+    val response = webResource.accept("application/json")
+        .header("Authorization", "Basic " + authStringEnc)
+        .get(classOf[ClientResponse])
 
     LOG.info("*** response ***" + response)
 
+    //deal with response
     var ret = new ServicePolicies
     var resp = new RESTResponse
     if (response != null && response.getStatus != 304)
@@ -104,4 +106,12 @@ class RangerAdminRestClientExtend extends RangerAdminRESTClient {
     ret
   }
 
+}
+
+object RangerAdminRestClientExtend {
+  private val REST_URL_POLICY_GET_FOR_SECURE_SERVICE_IF_UPDATED = "/service/plugins/secure/policies/download/"
+  private val REST_PARAM_LAST_KNOWN_POLICY_VERSION = "lastKnownVersion"
+  private val REST_PARAM_LAST_ACTIVATION_TIME = "lastActivationTime"
+  private val REST_PARAM_PLUGIN_ID = "pluginId"
+  private val REST_PARAM_CLUSTER_NAME = "clusterName"
 }

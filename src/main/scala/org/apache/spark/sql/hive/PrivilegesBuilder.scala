601c79f6bf3cc79622e287f360841731dec431f9
qiang.bi <qiang.bi@envision-digital.com>
2021-06-10 11:11:24 +0800

    remove log

--- src/main/scala/org/apache/spark/sql/hive/PrivilegesBuilder.scala
+++ src/main/scala/org/apache/spark/sql/hive/PrivilegesBuilder.scala
@@ -246,7 +246,6 @@ private[sql] object PrivilegesBuilder {
         addTableOrViewLevelObjs(c.table.identifier, outputObjs)
 
       case c: CreateFunctionCommand =>
-        LOG.info("*** CreateFunctionCommand {} . c.databaseName {} . c.functionName {} . ***" + c + ";" +c.databaseName + ";" +c.functionName)
         addDbLevelObjs(spark.catalog.currentDatabase, outputObjs)
         addFunctionLevelObjs(Some(spark.catalog.currentDatabase), c.functionName, outputObjs)
 
